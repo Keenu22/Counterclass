9d6de097ce4267aa9f84733a4750b96576574e14
Karina <karinamakkar18@gmail.com>
2024-09-29 16:06:09 +0530

    cmtd

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,12 +1,16 @@
+import React, { Component } from 'react';
 import './App.css';
 import Count from './Components/Count';
 
-function App() {
-  return (
-    <div className="App">
-      <Count/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Count />
+      </div>
+    );
+  }
 }
 
 export default App;
+
